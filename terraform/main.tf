248c519b89f913253d1212b0a971a20d0f401d90
Ivan Syrovoiskii <ivan.syrovoiskii@croud.com>
2022-08-22 16:21:01 +0100

    updated cicd action versions, added deployment shell script

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,41 +1,38 @@
 terraform {
   backend "gcs" {
-    bucket  = "croud_dev_pvt_megalista_terraform_state"
-    prefix  = "terraform/state"
+    prefix = "terraform/state"
   }
 }
 
 provider "google" {
-  project     = var.project
-  region        = var.region
-      scopes = [
-        "https://www.googleapis.com/auth/userinfo.email",
-        "https://www.googleapis.com/auth/compute",
-        "https://www.googleapis.com/auth/cloud-platform",
-        "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
-        "https://www.googleapis.com/auth/devstorage.full_control"
-    ]
+  project = var.project
+  region  = var.region
+  scopes = [
+    "https://www.googleapis.com/auth/userinfo.email",
+    "https://www.googleapis.com/auth/compute",
+    "https://www.googleapis.com/auth/cloud-platform",
+    "https://www.googleapis.com/auth/ndev.clouddns.readwrite",
+    "https://www.googleapis.com/auth/devstorage.full_control"
+  ]
 }
 
-
-
-module "megalista"{
-    source = "github.com/google/megalista//terraform?ref=2127bb8"
-    region = var.region
-    bq_ops_dataset = var.bq_ops_dataset
-    client_id = var.client_id
-    client_secret = var.client_secret
-    access_token = var.access_token
-    setup_json_url = var.setup_json_url
-    setup_firestore_collection = var.setup_firestore_collection
-    bucket_name = var.bucket_name
-    developer_token = var.developer_token
-    refresh_token = var.refresh_token
-    setup_sheet_id = var.setup_sheet_id
+module "megalista" {
+  source                     = "github.com/google/megalista//terraform?ref=2127bb8"
+  region                     = var.region
+  bq_ops_dataset             = var.bq_ops_dataset
+  client_id                  = var.client_id
+  client_secret              = var.client_secret
+  access_token               = var.access_token
+  setup_json_url             = var.config_bucket_name != "" ? "gs://${var.config_bucket_name}/configuration_sample.json" : ""
+  setup_firestore_collection = var.setup_firestore_collection
+  bucket_name                = var.bucket_name
+  developer_token            = var.developer_token
+  refresh_token              = var.refresh_token
+  setup_sheet_id             = var.setup_sheet_id
 }
 
 resource "google_storage_bucket" "config_storage" {
-  name          = var.config_bucket_name
-  location      = var.region
+  name                        = var.config_bucket_name
+  location                    = var.region
   uniform_bucket_level_access = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,51 +1,46 @@
 variable "bucket_name" {
-    type = string
-    description = "Google Cloud Storage Bucket to create to hold metadata"
+  type        = string
+  description = "Google Cloud Storage Bucket to create to hold metadata"
 }
 
 variable "bq_ops_dataset" {
-    type = string
-    description = "Auxliary bigquery dataset for Megalista operations to create"
+  type        = string
+  description = "Auxliary bigquery dataset for Megalista operations to create"
 }
 
 variable "developer_token" {
-    type = string
-    description = "Google Ads developer Token"
+  type        = string
+  description = "Google Ads developer Token"
 }
 
 variable "client_id" {
-    type = string
-    description = "OAuth Client Id"
+  type        = string
+  description = "OAuth Client Id"
 }
 
 variable "client_secret" {
-    type = string
-    description = "OAuth Client Secret"
+  type        = string
+  description = "OAuth Client Secret"
 }
 
 variable "access_token" {
-    type = string
-    description = "Access Token"
+  type        = string
+  description = "Access Token"
 }
 
 variable "refresh_token" {
-    type = string
-    description = "Refresh Token"
+  type        = string
+  description = "Refresh Token"
 }
 
 variable "setup_sheet_id" {
-    type = string
-    description = "Setup Sheet Id (leave blank if using JSON or Firestore)"
-}
-
-variable "setup_json_url" {
-    type = string
-    description = "URL for JSON configuration (leave blank if using Sheets or Firestore)"
+  type        = string
+  description = "Setup Sheet Id (leave blank if using JSON or Firestore)"
 }
 
 variable "setup_firestore_collection" {
-    type = string
-    description = "Setup Firestore collection (leave blank if using JSON or Sheets)"
+  type        = string
+  description = "Setup Firestore collection (leave blank if using JSON or Sheets)"
 }
 
 variable "region" {
@@ -59,6 +54,6 @@ variable "project" {
 }
 
 variable "config_bucket_name" {
-    type = string
-    description = "Google Cloud Storage Bucket to create to keep the config"
+  type        = string
+  description = "Google Cloud Storage Bucket to create to keep the config"
 }
