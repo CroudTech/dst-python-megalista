b3bf70a996ffa14bb58600d047615d86264fdc1d
Ivan Syrovoiskii <ivan.syrovoiskii@croud.com>
2022-08-23 16:08:26 +0100

    moved secrets to secret manager

--- terraform/main.tf
+++ terraform/main.tf
@@ -16,18 +16,39 @@ provider "google" {
   ]
 }
 
+data "google_secret_manager_secret_version" "client_id" {
+  secret   = "client_id"
+  version  = "1"
+}
+
+data "google_secret_manager_secret_version" "client_secret" {
+  secret   = "client_secret"
+  version  = "1"
+}
+
+data "google_secret_manager_secret_version" "access_token" {
+  secret   = "access_token"
+  version  = "1"
+}
+
+data "google_secret_manager_secret_version" "refresh_token" {
+  secret   = "refresh_token"
+  version  = "1"
+}
+
+
 module "megalista" {
   source                     = "github.com/google/megalista//terraform?ref=2127bb8"
   region                     = var.region
   bq_ops_dataset             = var.bq_ops_dataset
-  client_id                  = var.client_id
-  client_secret              = var.client_secret
-  access_token               = var.access_token
+  client_id                  = data.google_secret_manager_secret_version.client_id.secret_data
+  client_secret              = data.google_secret_manager_secret_version.client_secret.secret_data
+  access_token               = data.google_secret_manager_secret_version.access_token.secret_data
+  refresh_token              = data.google_secret_manager_secret_version.refresh_token.secret_data
   setup_json_url             = var.config_bucket_name != "" ? "gs://${var.config_bucket_name}/configuration_sample.json" : ""
   setup_firestore_collection = var.setup_firestore_collection
   bucket_name                = var.bucket_name
   developer_token            = var.developer_token
-  refresh_token              = var.refresh_token
   setup_sheet_id             = var.setup_sheet_id
 }
 
